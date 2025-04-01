fb0df1014a976b60d935225f84cc4d85f4031709
minorcell <minorcell6789@gmail.com>
2025-04-01 15:00:24 +0800

    upload hash

---
+++ Hash.js
@@ -0,0 +1,75 @@
+/**
+ * 字母异位词分组：
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ */
+
+var groupAnagrams = function (strs) {
+    let obj = {}
+
+    for (let str of strs) {
+        let sorted = str.split("").sort().join("")
+        if (!obj[sorted]) {
+            obj[sorted] = []
+        }
+        obj[sorted].push(str)
+    }
+
+    return Object.values(obj)
+};
+// let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+let strs = ["", ""];
+
+/**
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的
+ * 度。
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * 示例 1：
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 示例 2：
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * 示例 3：
+ * 输入：nums = [1,0,1,2]
+ * 输出：3
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    if (nums.length === 0) return 0;
+
+    const numSet = new Set(nums);
+    let maxLength = 0;
+
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentLength = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum++;
+                currentLength++;
+            }
+
+            maxLength = Math.max(maxLength, currentLength);
+        }
+    }
+
+    return maxLength;
+};
+
+console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
